d9f162f25a160bcee80173fbf8dd4799907ee51e
weizaishouex2010 <1114132381@qq.com>
2016-03-02 16:16:54 +0800

    修改了滑动事件的监听，遇到几个坑，布局不能随意布局，需要对应DoubelSlidingLayout里面的左右中的布局要求

--- doubleslidinglayout/src/main/java/com/huangzhiwei/doubleslidinglayout/DoubleSlidiingLayout.java
+++ doubleslidinglayout/src/main/java/com/huangzhiwei/doubleslidinglayout/DoubleSlidiingLayout.java
@@ -1,6 +1,7 @@
 package com.huangzhiwei.doubleslidinglayout;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -14,6 +15,31 @@ import android.widget.RelativeLayout;
  */
 public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouchListener {
 
+
+    /**
+     * 滑动状态的一种，表示未进行任何滑动。
+     */
+    public static final int DO_NOTHING = 0;
+
+    /**
+     * 滑动状态的一种，表示正在滑出左侧菜单。
+     */
+    public static final int SHOW_LEFT_MENU = 1;
+
+    /**
+     * 滑动状态的一种，表示正在滑出右侧菜单。
+     */
+    public static final int SHOW_RIGHT_MENU = 2;
+
+    /**
+     * 滑动状态的一种，表示正在隐藏左侧菜单。
+     */
+    public static final int HIDE_LEFT_MENU = 3;
+
+    /**
+     * 滑动状态的一种，表示正在隐藏右侧菜单。
+     */
+    public static final int HIDE_RIGHT_MENU = 4;
     /**
      * 设置滑动速度
      */
@@ -86,32 +112,6 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
     private RelativeLayout.LayoutParams contentLayoutParams;
 
 
-
-    /**
-     * 滑动状态的一种，表示未进行任何滑动。
-     */
-    public static final int DO_NOTHING = 0;
-
-    /**
-     * 滑动状态的一种，表示正在滑出左侧菜单。
-     */
-    public static final int SHOW_LEFT_MENU = 1;
-
-    /**
-     * 滑动状态的一种，表示正在滑出右侧菜单。
-     */
-    public static final int SHOW_RIGHT_MENU = 2;
-
-    /**
-     * 滑动状态的一种，表示正在隐藏左侧菜单。
-     */
-    public static final int HIDE_LEFT_MENU = 3;
-
-    /**
-     * 滑动状态的一种，表示正在隐藏右侧菜单。
-     */
-    public static final int HIDE_RIGHT_MENU = 4;
-
     /**
      * 记录当前的滑动状态
      */
@@ -154,6 +154,42 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
         touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
+
+    public void setScrollEvent(View bindView)
+    {
+        mBindView = bindView;
+        mBindView.setOnTouchListener(this);
+    }
+    /**
+     * 将界面从右侧菜单滚动到内容界面，滚动速度设定为30.
+     */
+    public void scrollToContentFromRightMenu() {
+        new RightMenuScrollTask().execute(30);
+    }
+
+    /**
+     * 将界面滚动到右侧菜单界面，滚动速度设定为-30.
+     */
+    public void scrollToRightMenu() {
+        new RightMenuScrollTask().execute(-30);
+    }
+
+    /**
+     * 将界面从左侧菜单滚动到内容界面，滚动速度设定为30.
+     */
+    private void scrollToContentFromLeftMenu() {
+        new LeftMenuScrollTask().execute(30);
+    }
+
+    /**
+     * 将界面滚动到左侧菜单界面，滚动速度设定为-30.
+     */
+    public void scrollToLeftMenu() {
+        new LeftMenuScrollTask().execute(-30);
+    }
+
+
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
@@ -162,22 +198,18 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
             leftMenuLayout = getChildAt(0);
             leftMenuLayoutParams = (MarginLayoutParams) leftMenuLayout.getLayoutParams();
 
-            contentLayout = getChildAt(1);
-            contentLayoutParams = (LayoutParams) leftMenuLayout.getLayoutParams();
-
-            rightMenuLayout = getChildAt(2);
+            rightMenuLayout = getChildAt(1);
             rightMenuLayoutParams = (MarginLayoutParams) rightMenuLayout.getLayoutParams();
+
+            contentLayout = getChildAt(2);
+            contentLayoutParams = (LayoutParams) contentLayout.getLayoutParams();
 
             contentLayoutParams.width = screenWidth;
             contentLayout.setLayoutParams(contentLayoutParams);
         }
     }
 
-    public void setScrollEvent(View bindView)
-    {
-        mBindView = bindView;
-        mBindView.setOnTouchListener(this);
-    }
+
 
 
 
@@ -185,8 +217,7 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         createVelocity(event);
-        switch (event.getAction())
-        {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 // 手指按下时，记录按下时的坐标
                 xDown = event.getRawX();
@@ -200,15 +231,97 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
                 // 手指移动时，对比按下时的坐标，计算出移动的距离。
                 int moveDistanceX = (int) (xMove - xDown);
                 int moveDistanceY = (int) (yMove - yDown);
-
-                // 检查当前的滑动状态  
+                // 检查当前的滑动状态
                 checkSlideState(moveDistanceX, moveDistanceY);
+                // 根据当前滑动状态决定如何偏移内容布局
+                switch (slideState) {
+                    case SHOW_LEFT_MENU:
+                        contentLayoutParams.rightMargin = -moveDistanceX;
+                        checkLeftMenuBorder();
+                        contentLayout.setLayoutParams(contentLayoutParams);
+                        break;
+                    case HIDE_LEFT_MENU:
+                        contentLayoutParams.rightMargin = -leftMenuLayoutParams.width - moveDistanceX;
+                        checkLeftMenuBorder();
+                        contentLayout.setLayoutParams(contentLayoutParams);
+                    case SHOW_RIGHT_MENU:
+                        contentLayoutParams.leftMargin = moveDistanceX;
+                        checkRightMenuBorder();
+                        contentLayout.setLayoutParams(contentLayoutParams);
+                        break;
+                    case HIDE_RIGHT_MENU:
+                        contentLayoutParams.leftMargin = -rightMenuLayoutParams.width + moveDistanceX;
+                        checkRightMenuBorder();
+                        contentLayout.setLayoutParams(contentLayoutParams);
+                    default:
+                        break;
+                }
                 break;
             case MotionEvent.ACTION_UP:
+                xUp = event.getRawX();
+                int upDistanceX = (int) (xUp - xDown);
+                if (isSliding) {
+                    // 手指抬起时，进行判断当前手势的意图
+                    switch (slideState) {
+                        case SHOW_LEFT_MENU:
+                            if (shouldScrollToLeftMenu()) {
+                                scrollToLeftMenu();
+                            } else {
+                                scrollToContentFromLeftMenu();
+                            }
+                            break;
+                        case HIDE_LEFT_MENU:
+                            if (shouldScrollToContentFromLeftMenu()) {
+                                scrollToContentFromLeftMenu();
+                            } else {
+                                scrollToLeftMenu();
+                            }
+                            break;
+                        case SHOW_RIGHT_MENU:
+                            if (shouldScrollToRightMenu()) {
+                                scrollToRightMenu();
+                            } else {
+                                scrollToContentFromRightMenu();
+                            }
+                            break;
+                        case HIDE_RIGHT_MENU:
+                            if (shouldScrollToContentFromRightMenu()) {
+                                scrollToContentFromRightMenu();
+                            } else {
+                                scrollToRightMenu();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                } else if (upDistanceX < touchSlop && isLeftMenuVisible) {
+                    // 当左侧菜单显示时，如果用户点击一下内容部分，则直接滚动到内容界面
+                    scrollToContentFromLeftMenu();
+                } else if (upDistanceX < touchSlop && isRightMenuVisible) {
+                    // 当右侧菜单显示时，如果用户点击一下内容部分，则直接滚动到内容界面
+                    scrollToContentFromRightMenu();
+                }
+                recycleVelocity();
                 break;
+        }
+        if (v.isEnabled()) {
+            if (isSliding) {
+                // 正在滑动时让控件得不到焦点
+                unFocusBindView();
+                return true;
+            }
+            if (isLeftMenuVisible || isRightMenuVisible) {
+                // 当左侧或右侧布局显示时，将绑定控件的事件屏蔽掉
+                return true;
+            }
+            return false;
         }
         return true;
     }
+
+
+
+
 
     private void checkSlideState(int moveDistanceX, int moveDistanceY) {
         if(isLeftMenuVisible)
@@ -229,22 +342,24 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
         }
         else
         {
-            if(!isSliding &&Math.abs(moveDistanceX)>=touchSlop && moveDistanceX>0)
+            if(!isSliding &&Math.abs(moveDistanceX)>=touchSlop && moveDistanceX>0
+                    && Math.abs(moveDistanceY) < touchSlop)
             {
                 isSliding = true;
                 slideState = SHOW_LEFT_MENU;
-                contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT,LEFT_OF);
+                contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT,0);
                 contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
                 contentLayout.setLayoutParams(contentLayoutParams);
 
                 leftMenuLayout.setVisibility(View.VISIBLE);
                 rightMenuLayout.setVisibility(View.GONE);
             }
-            else if(!isSliding && Math.abs(moveDistanceX)>=touchSlop && moveDistanceX <0)
+            else if(!isSliding && Math.abs(moveDistanceX)>=touchSlop && moveDistanceX <0
+                    && Math.abs(moveDistanceY) < touchSlop)
             {
                 isSliding = true;
                 slideState = SHOW_RIGHT_MENU;
-                contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,LEFT_OF);
+                contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,0);
                 contentLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
                 contentLayout.setLayoutParams(contentLayoutParams);
 
@@ -252,9 +367,49 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
                 rightMenuLayout.setVisibility(VISIBLE);
             }
 
-
         }
     }
+
+    /**
+     * 在滑动过程中检查左侧菜单的边界值，防止绑定布局滑出屏幕。
+     */
+    private void checkLeftMenuBorder() {
+        if (contentLayoutParams.rightMargin > 0) {
+            contentLayoutParams.rightMargin = 0;
+        } else if (contentLayoutParams.rightMargin < -leftMenuLayoutParams.width) {
+            contentLayoutParams.rightMargin = -leftMenuLayoutParams.width;
+        }
+    }
+
+    /**
+     * 在滑动过程中检查右侧菜单的边界值，防止绑定布局滑出屏幕。
+     */
+    private void checkRightMenuBorder() {
+        if (contentLayoutParams.leftMargin > 0) {
+            contentLayoutParams.leftMargin = 0;
+        } else if (contentLayoutParams.leftMargin < -rightMenuLayoutParams.width) {
+            contentLayoutParams.leftMargin = -rightMenuLayoutParams.width;
+        }
+    }
+
+
+
+    private boolean shouldScrollToLeftMenu() {
+        return xUp-xDown > leftMenuLayoutParams.width/2  || getVelocity()>SNAP_VELOCITY;
+    }
+    private boolean shouldScrollToRightMenu() {
+        return xDown-xUp > rightMenuLayoutParams.width/2 || getVelocity()>SNAP_VELOCITY;
+    }
+
+    private boolean shouldScrollToContentFromLeftMenu() {
+        return xDown - xUp > leftMenuLayoutParams.width/2 || getVelocity()>SNAP_VELOCITY;
+    }
+
+    private boolean shouldScrollToContentFromRightMenu() {
+        return xUp-xDown > rightMenuLayoutParams.width/2 || getVelocity()>SNAP_VELOCITY;
+    }
+
+
 
 
     /**
@@ -298,9 +453,123 @@ public class DoubleSlidiingLayout extends RelativeLayout implements View.OnTouch
     {
         if(mBindView!=null)
         {
-            mBindView.setEnabled(false);
+            mBindView.setPressed(false);
             mBindView.setFocusable(false);
             mBindView.setFocusableInTouchMode(false);
+        }
+    }
+
+
+    /**
+     * 左侧菜单是否完全显示出来，滑动过程中此值无效。
+     *
+     * @return 左侧菜单完全显示返回true，否则返回false。
+     */
+    public boolean isLeftLayoutVisible() {
+        return isLeftMenuVisible;
+    }
+
+    /**
+     * 右侧菜单是否完全显示出来，滑动过程中此值无效。
+     *
+     * @return 右侧菜单完全显示返回true，否则返回false。
+     */
+    public boolean isRightLayoutVisible() {
+        return isRightMenuVisible;
+    }
+
+
+    private class RightMenuScrollTask  extends AsyncTask<Integer,Integer,Integer>{
+        @Override
+        protected Integer doInBackground(Integer...speed) {
+            int leftMargin = contentLayoutParams.leftMargin;
+            // 根据传入的速度来滚动界面，当滚动到达边界值时，跳出循环。
+            while (true) {
+                leftMargin = leftMargin + speed[0];
+                if (leftMargin < -rightMenuLayoutParams.width) {
+                    leftMargin = -rightMenuLayoutParams.width;
+                    break;
+                }
+                if (leftMargin > 0) {
+                    leftMargin = 0;
+                    break;
+                }
+                publishProgress(leftMargin);
+                // 为了要有滚动效果产生，每次循环使线程睡眠一段时间，这样肉眼才能够看到滚动动画。
+                try {
+                    Thread.sleep(15);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (speed[0] > 0) {
+                isRightMenuVisible = false;
+            } else {
+                isRightMenuVisible = true;
+            }
+            isSliding = false;
+            return leftMargin;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            contentLayoutParams.leftMargin = values[0];
+            contentLayout.setLayoutParams(contentLayoutParams);
+            unFocusBindView();
+        }
+
+        @Override
+        protected void onPostExecute(Integer leftMargin) {
+            contentLayoutParams.leftMargin = leftMargin;
+            contentLayout.setLayoutParams(contentLayoutParams);
+        }
+    }
+
+    class LeftMenuScrollTask extends AsyncTask<Integer, Integer, Integer> {
+
+        @Override
+        protected Integer doInBackground(Integer... speed) {
+            int rightMargin = contentLayoutParams.rightMargin;
+            // 根据传入的速度来滚动界面，当滚动到达边界值时，跳出循环。
+            while (true) {
+                rightMargin = rightMargin + speed[0];
+                if (rightMargin < -leftMenuLayoutParams.width) {
+                    rightMargin = -leftMenuLayoutParams.width;
+                    break;
+                }
+                if (rightMargin > 0) {
+                    rightMargin = 0;
+                    break;
+                }
+                publishProgress(rightMargin);
+                // 为了要有滚动效果产生，每次循环使线程睡眠一段时间，这样肉眼才能够看到滚动动画。
+                try {
+                    Thread.sleep(15);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            if (speed[0] > 0) {
+                isLeftMenuVisible = false;
+            } else {
+                isLeftMenuVisible = true;
+            }
+            isSliding = false;
+            return rightMargin;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... rightMargin) {
+            contentLayoutParams.rightMargin = rightMargin[0];
+            contentLayout.setLayoutParams(contentLayoutParams);
+            unFocusBindView();
+        }
+
+        @Override
+        protected void onPostExecute(Integer rightMargin) {
+            contentLayoutParams.rightMargin = rightMargin;
+            contentLayout.setLayoutParams(contentLayoutParams);
         }
     }
 
